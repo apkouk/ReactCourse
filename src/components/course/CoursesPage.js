ca68f0ddb30161ff0a3e39914c2594eef90cbc5c
apkouk <cescorh@gmail.com>
2017-12-22 20:37:42 +0100

    packa

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
-class CoursesPage extends React.Component{
-  render(){
+class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  courseRow(course, index){
+    return <div key={index}>{course.title}</div>;
+  }
+
+  render() {
     return(
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
       </div>
     );
   }
 }
 
-export default CoursesPage;
+
+CoursesPage.propTypes = {
+  course: PropTypes.object.isRequired,
+  actions: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps){
+  return{
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return{
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps,  mapDispatchToProps)(CoursesPage);
