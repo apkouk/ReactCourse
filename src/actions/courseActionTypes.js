a2c4f2ad5227a3daebe0c0b101653e6b4453f1e7
apkouk <cescorh@gmail.com>
2018-01-02 18:35:47 +0100

    authors part

--- src/actions/courseActionTypes.js
+++
@@ -1,1 +0,0 @@
-export const LOAD_COURSES_SUCCESS = 'CREATE_COURSE';
--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,4 @@
-import * as types from './courseActionTypes';
+import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
 
